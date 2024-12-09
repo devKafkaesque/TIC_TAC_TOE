6e5797a6a6c122226d2b6d9462398fc620f9adbe
invisGhoul420 <parasharvishist@gmail.com>
2024-12-10 03:45:44 +0530

    added basic required structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Player from '/src/components/Player.jsx';
 
 function App() {
-  
+    return <main>
+        <div id='game-container'>
+            <ol id='players'>
+                <Player name= 'Me' symbol= 'O'/>
+                <Player name= 'Vishist' symbol= 'X'/>
+            </ol>
+            GAME BOARD
+        </div>
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+        LOG
+    </main>
+
 }
 
 export default App
