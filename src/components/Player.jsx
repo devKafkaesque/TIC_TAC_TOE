d842d4530019693e7e456d3ace8e87cc9a258430
invisGhoul420 <parasharvishist@gmail.com>
2024-12-10 03:46:04 +0530

    added Player tag config

---
+++ src/components/Player.jsx
@@ -0,0 +1,41 @@
+import {useState} from "react";
+
+export default function Player({name, symbol}) {
+    const [Name, setName] = useState(name);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleClick() {
+        setIsEditing((isEditing)=>!isEditing); // Disable editing mode
+    }
+
+    function handleInputChange(event) {
+        setName(event.target.value); // Update the state with input value
+    }
+
+    return (
+        <li>
+            {isEditing ? (
+                <>
+                    <span className='player'>
+                    <input
+                        placeholder="Enter Name"
+                        value={Name} // Link input value to state
+                        onChange={handleInputChange} // Update state on input change
+                    />
+                    </span>
+                    <span className="player-symbol">{symbol}</span>
+                    <button onClick={handleClick}>Save</button>
+                </>
+            ) : (
+                <>
+                    <span className='player'>
+                    <span className="player-name">{Name}</span>
+                    <span className="player-symbol">{symbol}</span>
+                    <button onClick={handleClick}>Edit</button>
+                    </span>
+                </>
+            )}
+
+        </li>
+    );
+}
