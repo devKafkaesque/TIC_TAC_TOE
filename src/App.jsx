0d6839edf5a5c4875a814d00166fa0673999031c
invisGhoul420 <parasharvishist@gmail.com>
2024-12-11 03:52:21 +0530

    added Gamebord and Player logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import Player from '/src/components/Player.jsx';
 import Gameboard from "./components/Gameboard.jsx";
+import {useState} from "react";
 
 function App() {
+    const [activePlayer, setActivePlayer] = useState('X');
+
+    function handleSelectSquare() {
+        setActivePlayer((curActivePlayer)=> curActivePlayer==='X' ? 'O' : 'X');
+    }
+
     return <main>
         <div id='game-container'>
-            <ol id='players'>
-                <Player name= 'Me' symbol= 'O'/>
-                <Player name= 'Vishist' symbol= 'X'/>
+            <ol id='players' className='highlight-player'>
+                <Player name= 'Me' symbol= 'X' isActive={activePlayer==='X'}/>
+                <Player name= 'Vishist' symbol= 'O' isActive={activePlayer==='O'}/>
             </ol>
 
             GAME BOARD
-            <Gameboard name= 'Game' symbol= 'O'/>
+            <Gameboard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
         </div>
 
         LOG
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,25 +1,41 @@
-export default function Gameboard() {
+import { useState } from "react";
 
+export default function Gameboard({onSelectSquare, activePlayerSymbol}) {
     const initialGameBoard = [
-        [null,null,null],
-        [null,null,null],
-        [null,null,null],
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
     ];
 
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    return <>
-        <ol id='game-board'>
-            {initialGameBoard.map((row, rowIndex) => (
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            // Create a deep copy of the game board
+            const updatedGameBoard = prevGameBoard.map((row) => [...row]);
+            if (!updatedGameBoard[rowIndex][colIndex]) { // Allow updates only if the square is empty
+                updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+            }
+            return updatedGameBoard;
+        });
+        onSelectSquare();
+    }
+
+    return (
+        <ol id="game-board">
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
-                        {row.map((playerSymbol, colIndex)=>(
+                        {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                                    {playerSymbol}
+                                </button>
                             </li>
                         ))}
                     </ol>
                 </li>
             ))}
         </ol>
-    </>
+    );
 }
